0bf54ca128532b8eefe1d8a2daa69f99b8520641
Jonathan Alberto Barrera <jonabarreraabc@gmail.com>
2026-01-22 20:21:02 -0600

    [add] codigo hecho en clase 22 enero

--- core/views.py
+++ core/views.py
@@ -15,3 +15,24 @@ def derick(request):
 
 def cvjona(request):
     return render(request, "core/cvjona.html")
+
+from django.shortcuts import render
+from .forms import ContactoForm
+
+def contacto_view(request):
+    if request.method == 'POST':
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            # Los datos ya pasaron las validaciones de front y back
+            nombre = form.cleaned_data['nombre']
+            email = form.cleaned_data['email']
+            mensaje = form.cleaned_data['mensaje']
+           
+            print(f"--- NUEVO MENSAJE ---")
+            print(f"Nombre: {nombre}\nEmail: {email}\nMensaje: {mensaje}")
+           
+            return render(request, 'core/formulario.html', {'form': form, 'success': True})
+    else:
+        form = ContactoForm()
+   
+    return render(request, 'core/formulario.html', {'form': form})
