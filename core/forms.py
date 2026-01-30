e6ae13e0a81f46a5a4b5ff3da0f0247874c76d39
Jonathan Alberto Barrera <jonabarreraabc@gmail.com>
2026-01-29 22:09:56 -0600

    [add] codigo clase lunes 26

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.core.exceptions import ValidationError
+import re
 
 class ContactoForm(forms.Form):
     nombre = forms.CharField(
-        min_length=3,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Tu nombre'})
+        min_length = 3,
+        widget= forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'Tu nombre','pattern' : '[a-zA-ZáéíóúÁÉÍÓÚ ]+','title':'Ingresa sólo letras'})
     )
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'correo@ejemplo.com'})
@@ -19,3 +20,10 @@ class ContactoForm(forms.Form):
         if "spam" in data.lower():
             raise ValidationError("No se permite contenido publicitario.")
         return data
+
+    def clean_nombre(self):
+        data = self.cleaned_data['nombre']
+        if not re.match('^[a-zA-ZáéíóúÁÉÍÓÚ]+$', data):
+            raise ValidationError("Ingresa sólo letras")
+        return data
+        
